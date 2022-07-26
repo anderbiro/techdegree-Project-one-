0cc1774119cd863492960aa5d80e6e91807ec541
anderbiro <109441678+anderbiro@users.noreply.github.com>
2022-07-26 08:39:07 -0500

    project 1 completed
    project one is completed

--- JS/script.js
+++ JS/script.js
@@ -9,25 +9,81 @@ project 1 - A Random Quote Generator
 
 /*** 
  * `quotes` array 
+ * here below are 5 quotes I chose to add in the array in the form of objects
+ 
 ***/
 
+let quotes = [
 
+  {quote:"Strive not to be a success, but rather to be of value", source:"Albert Einstein"},
+  
+  {quote:"Every strike brings me closer to the next home run", source:"Babe Ruth"},
+  
+  {quote:"Life isn't about getting and having, it's about giving and being.", source: "Kevin Kruse"},
+  
+  {quote:"It's the possibility of having a dream come true that makes life interesting.", source:"Paulo Coelho", citation: "The Alchemist"},                                                                 
+  
+  {quote: "the future depends on what you do today" , source: ["Mahatma Ghandi"] , year: ["1984"]},                                            
+  
+  ];
+  
 
-/***
+  /***
  * `getRandomQuote` function
+ * Here, I create this function to generate a random quote 
 ***/
 
+function getRandomQuote () {
+
+
+  const rand = Math.floor(Math.random()*quotes.length);
+
+  return quotes[rand];
+
+}
 
 
 /***
  * `printQuote` function
+ * This function will print the random quote generated in html.And so it can be displayed on the website
+ * quotes are composed of sources, citations and years as you can see in the original array "quotes"
 ***/
 
+function printQuote () {
 
+  let randq = getRandomQuote();
+ 
+  let html = `<p class="quote"> ${randq.quote} </p>  
+              <p class="source"> ${randq.source}  `
+              
+
+
+  if(Object.keys(randq).includes("citation")) {
+
+  html +=  `<span class="citation">${randq.citation} </span> </p> `;
+           
+    }
+
+
+  if(Object.keys(randq).includes("year")) {
+
+  html +=  `<span class="citation">${randq.year} </span> </p> `;
+
+
+    }
+
+
+    document.getElementById('quote-box').innerHTML = html; 
+
+}
+  
 
 /***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
+ * this object below creates a clickable button that will generate a new random quote each time
+   it is clicked.
+ 
 ***/
+
+
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
